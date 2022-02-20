b03fcd79a8b55dca80d1e62643e0ce8a7b75ff97
Isay Castaneda <isay.castaneda@bowheadhealth.com>
2022-02-19 23:33:14 -0600

    changes in profile mentor

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import AboutUs from "./modulos/AboutUs";
+import {MentorsProfileModule} from "./modulos/MentorsProfile"
+import Home from "./modulos/Home";
+import Navbar from "./modulos/Navbar";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import SingUp from "./modulos/SingUp";
+import Login from "./modulos/Login";
+
+
+function App() {
+
+
+  return (
+    <div>
+      <Router>
+        <Switch>
+
+          <Route path="/aboutUS">
+            <Navbar />
+            <AboutUs />
+          </Route>
+
+          <Route path="/mentorsProfile">
+            <Navbar />
+            <MentorsProfileModule />
+          </Route>
+
+          <Route path="/login">
+
+            <Login />
+          </Route>
+
+          <Route path="/singUp">
+            <Login esRegistro={true} />
+          </Route>
+
+          <Route path="/" exact>
+            <Navbar />
+            <Home />
+          </Route>
+
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
